7fed47d92f45608ae7799ea710dd81f396a1c25d
Her89 <her89ar@hotmail.com>
2016-11-25 17:59:29 -0300

    con database

--- src/main/java/com/accenture/Garcia/Hernan/ChickenTest/controller/WebSecurityConfig.java
+++ src/main/java/com/accenture/Garcia/Hernan/ChickenTest/controller/WebSecurityConfig.java
@@ -3,29 +3,30 @@ package com.accenture.Garcia.Hernan.ChickenTest.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired	
+	UserDetailsService userDetailsService;
+	
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-		auth
-			.inMemoryAuthentication()
-				.withUser("admin")
-					.password("admin")
-					.roles("ADMIN", "USER")
-					.and()
-				.withUser("user")
-					.password("user")
-					.roles("USER");
-	}
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {			 
+	 auth.userDetailsService(userDetailsService).passwordEncoder(passwordencoder());;
+		
+	}	
+
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -40,5 +41,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .and()
             .logout()
             	.permitAll();
+    }
+	
+	@Bean(name="passwordEncoder")
+    public PasswordEncoder passwordencoder(){
+    	return new BCryptPasswordEncoder();
     }
 }
